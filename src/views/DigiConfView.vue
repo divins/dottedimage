d9ac64d9cbd8a168bbbd877e0132284c19f5773a
Marc Divins Castellvi <marcdivc@gmail.com>
2021-04-17 17:31:49 +0200

    Adding Digital Conf (WIP)

---
+++ src/views/DigiConfView.vue
@@ -0,0 +1,15 @@
+<template>
+  <DigiConf />
+</template>
+
+<script>
+// @ is an alias to /src
+import DigiConf from "@/components/DigiConf.vue";
+
+export default {
+  name: "DigiConfView",
+  components: {
+    DigiConf
+  }
+};
+</script>
